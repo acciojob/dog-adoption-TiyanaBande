2af7e8392ce9f1fdb61afe55615a07175beccbb4
TiyanaBande <tanubande2001@gmail.com>
2024-07-29 12:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+const dogBreeds = [
+	{
+		name: "Golden Retriver",
+		imgSrc: "Golden_Retriver_img_link.jpg",
+		wikLink: "https://en.wikipedia.org/wiki/Golden_Retriver"
+	},
+	{
+		name: "Poodle",
+		imgSrc: "poodle_img_link.jpg",
+		wikiLink: "https://en.wikipedia.org/wiki/poodle"
+	}
+]
