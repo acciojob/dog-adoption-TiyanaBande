cbf2e6ce0fa1897e4429c6fc91b510ac7bd2eaf0
TiyanaBande <tanubande2001@gmail.com>
2024-07-29 16:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ const dogBreeds = [
 		imgSrc: "poodle_img_link.jpg",
 		wikiLink: "https://en.wikipedia.org/wiki/poodle"
 	}
-]
+];
+const dogBreedsList = document.getElement('dog-breeds-list');
+
+dogbreeds.forEach(breed =>{
+	const listItem = document.createElement('li');
+	const anchor = document.createElement('a');
+	const image = document.createElement('img');
+
+	anchor.href = breed.wikiLink;
+	anchor.target = "_blank";
+	image.src = breed.imgSrc;
+	image.alt = breed.name;
+
+	anchor.appendChild(image);
+	listitem.appendChild(anchor);
+	dogBreedsList.appendChild(listItem);
+});
